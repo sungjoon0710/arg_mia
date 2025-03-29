7ca30707929aed7d32e43a51939da744318fff49
sungjoon0710 <sungjoonpark0710@gmail.com>
2025-03-28 21:03:06 -0400

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [showNav, setShowNav] = useState(false);
+
   return (
     <div className="App">
+      <button className="nav-toggle" onClick={() => setShowNav(!showNav)}>
+        ☰
+      </button>
+      <nav className={`nav-bar ${showNav ? 'show' : 'hide'}`}>
+        <div className="nav-item">About</div>
+        <div className="nav-item">Products</div>
+        <div className="nav-item">Apply</div>
+        <div className="nav-item">Support</div>
+        <div className="nav-item">Login</div>
+      </nav>
       <header className="App-header">
         <p>REACH HIGHER</p>
         <h1>BABEL</h1>
