b0c3fd8b45093a4e69e7a646a99f99366b584ba3
sungjoon0710 <sungjoonpark0710@gmail.com>
2025-07-22 23:11:35 -0400

    back button

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 const Directory = () => {
   const folders = ['a', 'b', 'c'];
+  const [currentPath, setCurrentPath] = useState('');
+
+  const handleFolderClick = (folderName) => {
+    setCurrentPath(prevPath => prevPath + '/' + folderName);
+  };
+
+  const handleBackClick = () => {
+    const pathArray = currentPath.split('/').filter(folder => folder !== '');
+    pathArray.pop(); // Remove the last folder
+    setCurrentPath(pathArray.length > 0 ? '/' + pathArray.join('/') : '');
+  };
 
   return (
     <div className="directory-container">
       <div className="directory-header">
         <span className="directory-title">📁 DIRECTORY</span>
-        <span className="directory-path">C:\SYSTEM\FILES\</span>
+        <span className="directory-path">C:\SYSTEM\FILES{currentPath.replace(/\//g, '\\')}</span>
       </div>
       
       <div className="directory-content">
+        
         <div className="file-list">
-          {folders.map((folderName, index) => (
-            <div key={index} className="folder-item" onClick={() => console.log(`Opening folder: ${folderName}`)}>
-              <div className="folder-icon">📁</div>
-              <span className="folder-name">{folderName}</span>
+            {currentPath && (
+            <div className="folder-item" onClick={handleBackClick}>
+                <div className="folder-icon">📁</div>
+                <span className="folder-name">..</span>
             </div>
-          ))}
+            )}
+            {folders.map((folderName, index) => (
+                <div key={index} className="folder-item" onClick={() => handleFolderClick(folderName)}>
+                <div className="folder-icon">📁</div>
+                <span className="folder-name">{folderName}</span>
+                </div>
+            ))}
         </div>
       </div>
       
