6f9f47df3dc30c53bc13c68851f0b1fbdac36771
sungjoon0710 <sungjoonpark0710@gmail.com>
2025-08-25 10:36:45 +0900

    requested changes for prototype

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,8 +5,8 @@ function Game() {
   return (
     <div className="game-container">
       <div className="game-content">
-        <h2>MIAI - Our Game</h2>
-        <iframe title="mia-ai" frameborder="0" src="https://itch.io/embed-upload/14704208?color=333333" allowfullscreen="" width="1152" height="668"><a href="https://userjonas.itch.io/miai-prototype">Play MIAI prototype-005 on itch.io</a></iframe>
+        <h2>MIAI - Our Interrogation Simulator</h2>
+        <iframe frameborder="0" src="https://itch.io/embed-upload/14750309?color=000000" allowfullscreen="" width="1152" height="668"><a href="https://userjonas.itch.io/miai">Play MIAI on itch.io</a></iframe>
       </div>
     </div>
   );
